d447aa4b9e69f49c932dcf42cb91285d0f01d57b
iaracampos <iarac.rodrigues23@gmail.com>
2025-01-28 21:05:00 -0300

    feat: script p criar a tabela com resultados computacionais

--- MinMax_TSP.py
+++ MinMax_TSP.py
@@ -1,13 +1,13 @@
 import os
 import numpy as np
 from haversine import haversine, Unit
-from mst import *
+from mst_test import *  
+import time
 
 # Função para extrair coordenadas EUC_2D
 def euclidean_conversion(content):
     data = []
     in_node_section = False
-    
     for line in content.splitlines():
         if "NODE_COORD_SECTION" in line:
             in_node_section = True
@@ -19,14 +19,12 @@ def euclidean_conversion(content):
             if len(parts) >= 3:
                 _, x, y = parts
                 data.append((float(x), float(y)))
-    
     return data
 
 # Função para extrair coordenadas GEO
 def haversine_conversion(content):
     coordinates = []
     in_node_section = False
-    
     for line in content.splitlines():
         if "NODE_COORD_SECTION" in line:
             in_node_section = True
@@ -38,14 +36,12 @@ def haversine_conversion(content):
             if len(parts) >= 3:
                 _, latitude, longitude = parts
                 coordinates.append((float(latitude), float(longitude)))
-    
     return coordinates
 
 # Função para calcular matriz de adjacência
 def calculate_distance_matrix(nodes, weight_type):
     n = len(nodes)
-    matrix = np.zeros((n, n)) #inicializa com zero 
-    
+    matrix = np.zeros((n, n))
     for i in range(n):
         for j in range(i + 1, n):
             if weight_type == "EUC_2D":
@@ -56,10 +52,8 @@ def calculate_distance_matrix(nodes, weight_type):
                 dist = haversine(nodes[i], nodes[j], unit=Unit.KILOMETERS)
             else:
                 raise ValueError(f"Tipo de peso desconhecido: {weight_type}")
-            
             matrix[i][j] = dist
-            matrix[j][i] = dist  # assegurando que a matriz seja simetrica 
-    
+            matrix[j][i] = dist
     return matrix
 
 # Função para processar e transformar os dados
@@ -69,7 +63,6 @@ def data_transformation(content):
         for line in content.splitlines()
         if "EDGE_WEIGHT_TYPE" in line
     ]
-    
     if weight_type:
         weight_type = weight_type[0]
     else:
@@ -85,13 +78,11 @@ def data_transformation(content):
     distance_matrix = calculate_distance_matrix(nodes, weight_type)
     return distance_matrix
 
-# Função para ler as instâncias e gerar matrizes de adjacência
+# Função para ler as instâncias, gerar matrizes de adjacência e calcular a AGM
 def read_instances(path):
     output_dir = os.path.join(path, "adjacency_matrices")
-    os.makedirs(output_dir, exist_ok=True)  # Cria a pasta se não existir
-
+    os.makedirs(output_dir, exist_ok=True)
     files = [f for f in os.listdir(path) if f.endswith(".ins")]
-    
     for file in sorted(files):
         file_path = os.path.join(path, file)
         try:
@@ -102,35 +93,26 @@ def read_instances(path):
                 # Salvar a matriz de adjacência em arquivo .npy
                 output_file = os.path.join(output_dir, f"{os.path.splitext(file)[0]}_adjacency.npy")
                 np.save(output_file, distance_matrix)
-                
                 print(f"Matriz de adjacência gerada e salva para {file}")
+
+                # Chamar a função MST
+                origin = list(range(distance_matrix.shape[0]))  # Todos os nós
+                mst_edges, total_cost = mst(distance_matrix, origin)
+                print("Arestas:", mst_edges)
+                print("Custo Total:", total_cost)
         
         except Exception as e:
             print(f"Erro ao processar o arquivo {file}: {e}")
 
+# Função principal
 def main():
     input_path = "./instances"
-    # read_instances(input_path)
-    
-    # # Configurações para exibir matrizes inteiras
 
-    # np.set_printoptions(
-    #     threshold=np.inf,  # Exibe todos os elementos
-    #     linewidth=200,     # Ajusta a largura da linha
-    #     suppress=True      # Evita a notação científica para números pequenos
-    # )
-
-    matrix = np.load("./instances/adjacency_matrices/01_adjacency.npy")
-    print(matrix)
-
-    # Função para pegar os vizinhos mais próximos de um ponto de origem
-    # TODO: Esta função está retornando na verdade os vizinhos mais proximos dos dois indices que possuem maior distância entre eles
-    # closest_neighbors_data = closest_neighbors(matrix)
-
-    # Essa linha é para poder pegar o primeiro indice a ser retornado, e encontrar os vizinhos mais próximos a partir dele
-    # cluster = next(iter(closest_neighbors_data))
-
-    # print(mst(matrix,closest_neighbors_data[cluster]))
+    start_time = time.time()
+    read_instances(input_path)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Tempo total de execução: {execution_time:.4f} segundos")
 
 
 if __name__ == "__main__":
